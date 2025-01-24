fce1495a22b3246470c17577c671c720fd6972a6
younminchan <dbsalscks0@naver.com>
2025-01-24 11:09:57 +0900

    테스트 프로젝트 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.lotte.dcx.security"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lotte.dcx.security"
@@ -40,9 +40,13 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+
+//    implementation(files("libs/lotte_dcx_security-debug.aar"))
     implementation(files("libs/lotte_dcx_security-release.aar"))
 }
---
+++ app/src/main/java/com/lotte/dcx/security/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.lotte.dcx.security
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //1. 루팅체크
+        if (RootDetection().isDeviceRooted(this@MainActivity)) {
+            Handler().postDelayed({
+                Toast.makeText(this@MainActivity, "루팅이 감지되었습니다.", Toast.LENGTH_SHORT).show()
+                finishAffinity()
+            }, 1000)
+        }
+
+        //2. 무결성 Signkey 가져오기
+        var appSignatrue = RootDetection().getAppSignature(this@MainActivity)
+        SLog.i("YMC", "appSignatrue: $appSignatrue")
+
+        //3. 캡처방지
+        RootDetection().setScreenProtect(this@MainActivity, result = { result ->
+            SLog.i("YMC", "appSignatrue: ${if(result) "성공" else "실패"}")
+            Toast.makeText(this@MainActivity, "setScreenProtect ${if(result) "성공" else "실패"}", Toast.LENGTH_SHORT).show()
+        })
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            RootDetection().removeScreenProtect(this@MainActivity, result = { result ->
+                SLog.i("YMC", "removeScreenProtect: ${if(result) "성공" else "실패"}")
+                Toast.makeText(this@MainActivity, "removeScreenProtect ${if(result) "성공" else "실패"}", Toast.LENGTH_SHORT).show()
+            })
+        },3000)
+    }
+}
