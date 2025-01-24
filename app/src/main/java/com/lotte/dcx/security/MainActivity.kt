b40764b58c2287191dfa34ef73769b29f17a697c
younminchan <dbsalscks0@naver.com>
2025-01-24 13:54:45 +0900

    라이브러리 테스트코드 일부 수정

--- app/src/main/java/com/lotte/dcx/security/MainActivity.kt
+++ app/src/main/java/com/lotte/dcx/security/MainActivity.kt
@@ -3,11 +3,10 @@ package com.lotte.dcx.security
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        //1. 루팅체크
+        /** 1. 루팅체크 */
         if (RootDetection().isDeviceRooted(this@MainActivity)) {
             Handler().postDelayed({
                 Toast.makeText(this@MainActivity, "루팅이 감지되었습니다.", Toast.LENGTH_SHORT).show()
@@ -23,19 +22,19 @@ class MainActivity : AppCompatActivity() {
             }, 1000)
         }
 
-        //2. 무결성 Signkey 가져오기
+        /** 2. 무결성 Signkey 가져오기 */
         var appSignatrue = RootDetection().getAppSignature(this@MainActivity)
-        SLog.i("YMC", "appSignatrue: $appSignatrue")
+        Log.i("YMC", "appSignatrue: $appSignatrue")
 
-        //3. 캡처방지
+        /** 3. 캡처방지 */
         RootDetection().setScreenProtect(this@MainActivity, result = { result ->
-            SLog.i("YMC", "appSignatrue: ${if(result) "성공" else "실패"}")
+            Log.i("YMC", "appSignatrue: ${if(result) "성공" else "실패"}")
             Toast.makeText(this@MainActivity, "setScreenProtect ${if(result) "성공" else "실패"}", Toast.LENGTH_SHORT).show()
         })
 
         Handler(Looper.getMainLooper()).postDelayed({
             RootDetection().removeScreenProtect(this@MainActivity, result = { result ->
-                SLog.i("YMC", "removeScreenProtect: ${if(result) "성공" else "실패"}")
+                Log.i("YMC", "removeScreenProtect: ${if(result) "성공" else "실패"}")
                 Toast.makeText(this@MainActivity, "removeScreenProtect ${if(result) "성공" else "실패"}", Toast.LENGTH_SHORT).show()
             })
         },3000)
