bb8f471a46d5e802e32bd0f3a71a4dbc1a2ca121
younminchan <dbsalscks0@naver.com>
2025-02-03 14:51:18 +0900

    lib파일 현재시점에서 최신화 파일로 테스트 진행

--- app/src/main/java/com/lotte/dcx/security/MainActivity.kt
+++ app/src/main/java/com/lotte/dcx/security/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.dcx.security.RootDetection
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
